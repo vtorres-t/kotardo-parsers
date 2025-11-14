118796b69e510d78214bdba0b34ae0f76a324c3f
Vicente <vicente@per>
2025-11-14 10:35:26 +0100

    Tresdaos broken

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/mangareader/es/Tresdaos.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/mangareader/es/Tresdaos.kt
@@ -1,11 +1,13 @@
 package org.koitharu.kotatsu.parsers.site.mangareader.es
 
+import org.koitharu.kotatsu.parsers.Broken
 import org.koitharu.kotatsu.parsers.MangaLoaderContext
 import org.koitharu.kotatsu.parsers.MangaSourceParser
 import org.koitharu.kotatsu.parsers.model.MangaListFilterCapabilities
 import org.koitharu.kotatsu.parsers.model.MangaParserSource
 import org.koitharu.kotatsu.parsers.site.mangareader.MangaReaderParser
 
+@Broken
 @MangaSourceParser("TRESDAOS", "Tresdaos", "es")
 internal class Tresdaos(context: MangaLoaderContext) :
 	MangaReaderParser(context, MangaParserSource.TRESDAOS, "threedaos.zdrz.xyz", 20, 10) {
