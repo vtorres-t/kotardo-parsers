8e15c66c8980a6c92f70507b03796dcc31d6131d
Vicente <vicente@per>
2025-11-21 11:05:13 +0100

    Add MANGAREADERPRO

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/es/MangaReaderpro.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/es/MangaReaderpro.kt
@@ -2,12 +2,12 @@ package org.koitharu.kotatsu.parsers.site.madara.es
 
 import org.koitharu.kotatsu.parsers.MangaLoaderContext
 import org.koitharu.kotatsu.parsers.MangaSourceParser
-import org.koitharu.kotatsu.parsers.model.MangaSource
+import org.koitharu.kotatsu.parsers.model.MangaParserSource
 import org.koitharu.kotatsu.parsers.site.madara.MadaraParser
 
 @MangaSourceParser("MANGAREADERPRO", "Manga Reader Pro", "es")
 internal class MangaReaderpro(context: MangaLoaderContext) :
-    MadaraParser(context, MangaSource.MANGAREADERPRO, "mangareaderpro.com", 10) {
+    MadaraParser(context, MangaParserSource.MANGAREADERPRO, "mangareaderpro.com", 10) {
 
     override val datePattern = "MM/dd/yyyy"
 }
